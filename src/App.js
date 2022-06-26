a002db5c9cf9cdd6c10d8a98c19ce654a7d0ce1b
Tonii1993 <96661796+Tonii1993@users.noreply.github.com>
2022-06-26 16:31:36 +0200

    finished blog post

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './NavBar';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFoundPage from './NotFoundPage';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route path='/' element={ <Home/> }/>
+            <Route path='/create' element={ <Create /> }/>
+            <Route path='/blogs/:id' element={<BlogDetails />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
